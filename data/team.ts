42fcfabd5e35b0637ce24bba6d0550ad4f8a618f
Paul Merupu <paulmerupu@Pauls-M3-Pro-MacBook-Pro.local>
2023-12-18 08:18:52 -0500

    Initial commit.

---
+++ data/team.ts
@@ -0,0 +1,36 @@
+type TeamMember = {
+    name: String
+}
+
+const team : Array<TeamMember> = [
+    {
+        name: "Liam Smith"
+    },
+    {
+        name: "Ava Johnson"
+    },
+    {
+        name: "Noah Brown"
+    },
+    {
+        name: "Olivia Davis"
+    },
+    {
+        name: "William Garcia"
+    },
+    {
+        name: "Sophia Rodriguez"
+    },
+    {
+        name: "James Martinez"
+    },
+    {
+        name: "Isabella Wilson"
+    },
+    {
+        name: "Benjamin Anderson"
+    },
+    {
+        name: "Jenny Thomas"
+    },
+]
